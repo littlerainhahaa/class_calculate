92f9ca42d08a21bbe8dc70330b6d67d0bfe3fd7d
Yu-ting, Hsu <110767545+littlerainhahaa@users.noreply.github.com>
2023-07-20 18:13:02 +0800

    Update factorial_calculate.py

--- factorial_calculate.py
+++ factorial_calculate.py
@@ -2,12 +2,11 @@ n = int(input("> "))
 a = 1
 b = 1
 cal = str(a)
-if a < n:
-    for i in range(1, n+1):
-        print(str(i)+"!", "=", cal, "=", b)
-        a += 1
-        b *= (i+1)
-        cal += "x" + str(a)
+for i in range(1, n+1):
+    print(str(i)+"!", "=", cal, "=", b)
+    a += 1
+    b *= (i+1)
+    cal += "x" + str(a)
 
 
 
